bb01f8b909458baa30f5d301989b08867b24663f
Abdullah <abdullahyahaya46@gmail.com>
2022-12-25 02:25:05 +0100

    Created  New Sepearate Links

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-import { createUrlHandler } from '../controller/url.controller';
+import { createUrlHandler, retriveLink, deleteLink, getAllLinks } from '../controller/url.controller';
 import requireUser from '../middleware/requireUser';
 
 const router = express.Router();
 
 // Create a link
 router.post('/url-shortener', requireUser, createUrlHandler);
+
+// Get All Links
+router.get('/url-shortener', requireUser, getAllLinks);
+
+// Fetch Just A Link
+router.get('/url-shortner/:url', requireUser, retriveLink);
+
+// Delete A Link
+router.delete('/url-shortner/:id', requireUser, deleteLink);
 
 export default router;
