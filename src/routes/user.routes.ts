223064ce9f9152fa716e6b19411db1dfd199b7c7
Abdullah <abdullahyahaya46@gmail.com>
2022-12-29 19:43:16 +0100

    Made Necessary Adjustments on routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUserHandler, deleteSessionHandler, loginUserHandler } from '../controller/user.controller';
+import { createUserHandler, deleteSessionHandler, getUserProfile, loginUserHandler, updateUserProfile } from '../controller/user.controller';
 import requireUser from '../middleware/requireUser';
 import validateResources from '../middleware/validateResource';
 import { createUserSchema, loginUserSchema } from '../schema/user.schema';
@@ -13,8 +13,10 @@ router.post('/users/signup', validateResources(createUserSchema), createUserHand
 router.post('/users/login', validateResources(loginUserSchema), loginUserHandler);
 
 // Check User Profile
+router.get('/users/profile', requireUser, getUserProfile);
 
 // User Update Account
+router.patch('/users/profile', requireUser, updateUserProfile);
 
 // User Logout
 router.delete('/users/logout', requireUser, deleteSessionHandler);
