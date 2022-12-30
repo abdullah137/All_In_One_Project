81f6a5be0940d5a4a862901e6fce3aeb9119cf14
Abdullah <abdullahyahaya46@gmail.com>
2022-12-30 19:21:41 +0100

    Routes Index (Modified)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,24 +2,16 @@ import express, { Request, Response } from 'express';
 import user from './user.routes';
 import shortner from './url.routes';
 import bgRemover from './bg.routes';
+import youtubeDownloader from './youtube.routes';
+import { homePage, healthCheck } from '../controller/base.controller';
 
 const router = express.Router();
 
 // Welcome Endpoint
-router.get('/', (req: Request, res: Response) => {
-    return res.json({
-        msg: 'Welcome to the all in one project',
-        status: 200
-    });
-});
+router.get('/', homePage);
 
 // healthcheck status
-router.get('/healthcheck', (req: Request, res: Response) => {
-    return res.json({
-        msg: 'Welcome to the all in one project',
-        status: 200
-    });
-});
+router.get('/healthcheck', healthCheck);
 
 // User Routes
 router.use(user);
@@ -29,5 +21,8 @@ router.use(shortner);
 
 // Background Remover Routes
 router.use(bgRemover);
+
+// Youtube downloader Routes
+router.use(youtubeDownloader);
 
 export default router;
