fd32ce9370e1aaea54e5ef077d0c7cb5325d383c
Abdullah <abdullahyahaya46@gmail.com>
2022-12-31 15:58:32 +0100

    Created A New Function for Random Strings

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,4 +7,14 @@ function isValidHttpUrl(string: string): boolean {
     }
 }
 
-export { isValidHttpUrl };
+function generateRandomStrings(length: number): string {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+export { isValidHttpUrl, generateRandomStrings };
