30940780fb0ea24a74e18ad9714a5f6bd97bfc5b
Abdullah <abdullahyahaya46@gmail.com>
2023-01-02 21:56:55 +0100

    Created A New Service For Forget Password

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,6 +1,7 @@
 import { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
 import { omit } from 'lodash';
 import UserModel, { UserDocument, UserInput } from '../models/user.model';
+import forgetPasswordModel, { forgetPasswordDocument, forgetPasswordType } from '../models/forgetPassword.model';
 
 export async function createUser(input: UserInput) {
     try {
@@ -51,6 +52,34 @@ export async function findUser(query: FilterQuery<UserDocument>) {
 export async function updateProfile(query: FilterQuery<UserDocument>, update: UpdateQuery<UserDocument>, options: QueryOptions) {
     try {
         return UserModel.findOneAndUpdate(query, update, options);
+    } catch (e: any) {
+        throw new Error(e);
+    }
+}
+
+export async function findResetLink(query: FilterQuery<forgetPasswordDocument>) {
+    try {
+        return forgetPasswordModel.findOne(query).lean();
+    } catch (e: any) {
+        throw new Error(e);
+    }
+}
+
+export async function createResetLink(info: forgetPasswordType) {
+    try {
+        return await forgetPasswordModel.create(info);
+    } catch (e: any) {
+        throw new Error(e);
+    }
+}
+
+export async function updateResetLink(id: string, link: string) {
+    try {
+        return await forgetPasswordModel.findByIdAndUpdate(id, {
+            $set: {
+                resetLink: link
+            }
+        });
     } catch (e: any) {
         throw new Error(e);
     }
