33a4e7cac5752a0d871094c01a909c14e5d300b7
Abdullah <abdullahyahaya46@gmail.com>
2023-01-02 21:54:40 +0100

    Modified the Index Page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,20 +3,17 @@ import user from './user.routes';
 import shortner from './url.routes';
 import bgRemover from './bg.routes';
 import youtubeDownloader from './youtube.routes';
-import { homePage, healthCheck } from '../controller/base.controller';
+import { homePage } from '../controller/base.controller';
 
 const router = express.Router();
 
 // Welcome Endpoint
 router.get('/', homePage);
 
-// healthcheck status
-router.get('/healthcheck', healthCheck);
-
 // User Routes
 router.use(user);
 
-// Url-Shorther Routes
+// Url-Shortner Routes
 router.use(shortner);
 
 // Background Remover Routes
