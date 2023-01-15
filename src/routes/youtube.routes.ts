61f589e1737a8cf985e3fbb9d169762f60735919
Abdullah <abdullahyahaya46@gmail.com>
2023-01-15 17:50:21 +0100

    Youtube route demystified

--- src/routes/youtube.routes.ts
+++ src/routes/youtube.routes.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
-import { getyoubueLink } from '../controller/youtube.controller';
+import { getyoubueLink, downloadFile } from '../controller/youtube.controller';
 
 const router = express.Router();
 
 // Youtube downloader is here
 router.get('/youtube', getyoubueLink);
+
+router.get('/youtube/:link', downloadFile);
 
 export default router;
