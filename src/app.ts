3f199c25b96d4fdaaf4708c61a8ccd6124147c06
abdullah137 <abdullahyahaya46@gmail.com>
2023-05-01 12:16:59 +0100

    refactor: change code logic

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import connectDatabase from './database/connect';
 import deserializeUser from './middleware/deserializeUser';
 import router from './routes';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
@@ -20,13 +21,31 @@ app.use(router);
 // Handling cors request
 app.use(
     cors({
-        origin: true,
-        methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
-        credentials: true,
-        maxAge: 3600
+        origin(requestOrigin, callback) {
+            // allow all for now
+            return callback(null, true);
+
+            // switch to this when you want to restrict the origin
+            // if (requestOrigin === undefined) {
+            //     return callback(null, true);
+            // }
+            // const o = new URL(requestOrigin);
+            // // check if TLD is in the list of allowed TLDs
+            // if (env.allowedTLDs.includes(o.hostname)) {
+            //   return callback(null, true);
+            // }
+            // return callback(new Error('Not allowed by CORS'));
+        },
+        optionsSuccessStatus: 204
     })
 );
-
+app.use(
+    helmet({
+        frameguard: {
+            action: 'deny' // change to 'sameorigin' if you want to allow iframes
+        }
+    })
+);
 // Error Handlers for 404
 app.all('*', (req: Request, res: Response) => {
     return res.status(404).json({
